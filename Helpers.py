63e56c7e158132befa91dcd839fd98f48db1afaf
Codeblogger <m.furkangulsen@hotmail.com>
2020-08-05 14:44:19 +0300

    document scanner

--- Helpers.py
+++ Helpers.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 class Helpers:
 	def __init__(self):
@@ -27,3 +28,40 @@ class Helpers:
 		else:
 			raise Exception('The length of the contour must be 2 or 3.')
 		return cnts
+
+
+	def orders(pts):
+		rect = np.zeros((4, 2), dtype = "float32")
+		s = pts.sum(axis = 1)
+
+		rect[0] = pts[np.argmin(s)]
+		rect[2] = pts[np.argmax(s)]
+
+		diff = np.diff(pts, axis = 1)
+		rect[1] = pts[np.argmin(diff)]
+		rect[3] = pts[np.argmax(diff)]
+
+		return rect
+
+	def transform(image, pts):
+		rect = Helpers.orders(pts)
+		(tl, tr, br, bl) = rect
+
+		widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+		widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+		maxWidth = max(int(widthA), int(widthB))
+
+		heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+		heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+		maxHeight = max(int(heightA), int(heightB))
+
+		dst = np.array([
+			[0, 0],
+			[maxWidth - 1, 0],
+			[maxWidth - 1, maxHeight - 1],
+			[0, maxHeight - 1]], dtype = "float32")
+
+		M = cv2.getPerspectiveTransform(rect, dst)
+		warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+
+		return warped
--- main.py
+++ main.py
@@ -8,43 +8,6 @@ from PIL import Image
 import base64
 from Helpers import *
 
-def orders(pts):
-	rect = np.zeros((4, 2), dtype = "float32")
-	s = pts.sum(axis = 1)
-
-	rect[0] = pts[np.argmin(s)]
-	rect[2] = pts[np.argmax(s)]
-
-	diff = np.diff(pts, axis = 1)
-	rect[1] = pts[np.argmin(diff)]
-	rect[3] = pts[np.argmax(diff)]
-
-	return rect
-
-def transform(image, pts):
-	rect = orders(pts)
-	(tl, tr, br, bl) = rect
-
-	widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
-	widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
-	maxWidth = max(int(widthA), int(widthB))
-
-	heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
-	heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
-	maxHeight = max(int(heightA), int(heightB))
-
-	dst = np.array([
-		[0, 0],
-		[maxWidth - 1, 0],
-		[maxWidth - 1, maxHeight - 1],
-		[0, maxHeight - 1]], dtype = "float32")
-
-	M = cv2.getPerspectiveTransform(rect, dst)
-	warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
-
-	return warped
-
-
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 def allowed_file(filename):
@@ -91,7 +54,7 @@ def upload_image():
 
 		cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 2)
 
-		warped = transform(orig, screenCnt.reshape(4, 2) * ratio)
+		warped = Helpers.transform(orig, screenCnt.reshape(4, 2) * ratio)
 
 		img = cv2.cvtColor(warped, cv2.COLOR_BGR2RGB)
 		file_object = io.BytesIO()
